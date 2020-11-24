39b5b5ea08cfbf9b0887ec4083b9075a7009b619
shokichi627 <shokichi627@gmail.com>
2020-11-24 21:59:36 +0900

    .vscodeをgitignore3

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:50',
+            'body' => 'required|max:100',
+            'category_id' => 'required|integer',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            "required" => "必須項目です。",
+            "title.max" => "50文字以内で入力してください。",
+            "body.max" => "100文字以内で入力してください。"
+        ];
+    }
+}
